e5ec3c4110c7b01dc8980d7eed1ba4a9202cbf2a
BrandonHextall <122377245+BrandonHextall@users.noreply.github.com>
2023-01-10 14:41:34 +0000

    Add files via upload

---
+++ HTML Clicker/MK2 snippets/dynamic upgrade creator.js
@@ -0,0 +1,145 @@
+function check_items()
+{
+    if (owned_cursors == 1) //cu1
+    {
+        create_src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f49d0a73-3908-4ce9-a11f-c9fc7cb20745/dfg7l3i-492cbf45-d9e6-4410-8bf9-e4d43640880d.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y0OWQwYTczLTM5MDgtNGNlOS1hMTFmLWM5ZmM3Y2IyMDc0NVwvZGZnN2wzaS00OTJjYmY0NS1kOWU2LTQ0MTAtOGJmOS1lNGQ0MzY0MDg4MGQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ocYAeTC_4wYKfnhC0FthKj9e-RSTCR-7fQMbP3GqbIo"
+        create_id="1"
+        create_cur()
+    }
+    else if (owned_cursors == 10) //cu2
+    {
+        create_src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f49d0a73-3908-4ce9-a11f-c9fc7cb20745/dfg7l45-b6012551-6c42-4aff-9965-d4309304b1ca.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y0OWQwYTczLTM5MDgtNGNlOS1hMTFmLWM5ZmM3Y2IyMDc0NVwvZGZnN2w0NS1iNjAxMjU1MS02YzQyLTRhZmYtOTk2NS1kNDMwOTMwNGIxY2EucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.lx7FCah727cAvS-hWnK2vUxit-_dhfzVXhywmjBJA1Q"
+        create_id="2"
+        create_upgrade()
+    }
+    else if (owned_employees == 1) //eu1
+    {
+        create_src=""
+        create_id="3"
+        create_upgrade()
+    }
+    else if (owned_cursors == 25) //cu3
+    {
+        create_src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f49d0a73-3908-4ce9-a11f-c9fc7cb20745/dfg7l4f-1eb38938-dec3-4948-9f5f-15c3a10e21e6.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y0OWQwYTczLTM5MDgtNGNlOS1hMTFmLWM5ZmM3Y2IyMDc0NVwvZGZnN2w0Zi0xZWIzODkzOC1kZWMzLTQ5NDgtOWY1Zi0xNWMzYTEwZTIxZTYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.rsh7krc4cf2lptjVooXhW_VBBXxr8S5jxERRdEzupDY"
+        create_id="4"
+        create_upgrade()
+    }
+    else if (owned_employees == 10) //eu2
+    {
+        create_src=""
+        create_id="5"
+        create_upgrade()
+    }
+    else if (owned_cursors == 50) //cu4
+    {
+        create_src=""
+        create_id="6"
+        create_upgrade()
+    }
+    else if (owned_cursors == 100) //cu5
+    {
+        create_src=""
+        create_id="7"
+        create_upgrade()
+    }
+    else if (owned_employees == 25) //eu3
+    {
+        create_src=""
+        create_id="8"
+        create_upgrade()
+    }
+    else if (owned_cursors == 150) //cu6
+    {
+        create_src=""
+        create_id="9"
+        create_upgrade()
+    }
+    else if (owned_employees == 50) // eu4
+    {
+        create_src=""
+        create_id="10"
+        create_upgrade()
+    }
+    else if (owned_cursors == 200) //cu7
+    {
+        create_src=""
+        create_id="11"
+        create_upgrade()
+    }
+    else if (owned_employees == 100) //eu5
+    {
+        create_src=""
+        create_id="12"
+        create_upgrade()
+    }
+
+    else if (owned_cursors == 250) //cu8
+    {
+        create_src=""
+        create_id="13"
+        create_upgrade()
+    }
+    else if (owned_employees == 150) //eu6
+    {
+        create_src=""
+        create_id="14"
+        create_upgrade()
+    }
+    else if (owned_cursors == 300) //cu9
+    {
+        create_src=""
+        create_id="15"
+        create_upgrade()
+    }
+    else if (owned_employees == 200) //eu7
+    {
+        create_src=""
+        create_id="16"
+        create_upgrade()
+    }
+    else if (owned_employees == 250) //eu8
+    {
+        create_src=""
+        create_id="17"
+        create_upgrade()
+    }
+    else if (owned_cursors == 400) //cu10
+    {
+        create_src=""
+        create_id="18"
+        create_upgrade()
+    }
+    else if (owned_employees == 300) //eu9
+    {
+        create_src=""
+        create_id="19"
+        create_upgrade()
+    }
+    else if (owned_cursors == 500) //cu11
+    {
+        create_src=""
+        create_id="20"
+        create_upgrade()
+    }
+    else if (owned_employees == 400) //eu10
+    {
+        create_src=""
+        create_id="21"
+        create_upgrade()
+    }
+    else if (owned_employees == 500) //eu11
+    {
+        create_src=""
+        create_id="22"
+        create_upgrade()
+    }
+}
+function create_upgrade()
+{
+    var create = document.createElement("input")
+    create.type="image"
+    create.src=create_src
+    create.id=create_id
+    create.style.display="inline"
+    document.getElementsByTagName("div")["upgrades"].appendChild(create)
+}
