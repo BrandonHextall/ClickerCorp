cc654c28be3528718b8c749742bb59b5f5ae3faf
BrandonHextall <122377245+BrandonHextall@users.noreply.github.com>
2023-01-10 14:47:56 +0000

    Add files via upload

---
+++ Clicker Corp/Practices/dynamically create variables.js
@@ -0,0 +1,11 @@
+function createVariables(){
+    var accounts = [];
+  
+    for (var i = 0; i <= 20; ++i) {
+        accounts[i] = "whatever";
+    }
+  
+    return accounts;
+    console.log(accounts)
+  }
+  createVariables()
