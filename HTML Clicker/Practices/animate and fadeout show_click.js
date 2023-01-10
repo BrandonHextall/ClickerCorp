68f9b927443c8f5920bb7afb190eceb56efa5b09
BrandonHextall <122377245+BrandonHextall@users.noreply.github.com>
2023-01-10 14:46:59 +0000

    Delete HTML Clicker directory

--- HTML Clicker/Practices/animate and fadeout show_click.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var add_pos = $(clone).position().top +100
-    console.log(add_pos)
-    $(clone).animate({top: add_pos-200},1500)
-    console.log(add_pos)
-    $(clone).fadeOut(1500)
-    element = document.getElementById(click_id)
-})
